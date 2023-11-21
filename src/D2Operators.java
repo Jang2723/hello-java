533217de3bc09cae4e77b914bfc43f92ccca2635
Jang2723 <jangsb46@gmail.com>
2023-11-21 10:45:39 +0900

    update files

---
+++ src/D2Operators.java
@@ -0,0 +1,10 @@
+public class D2Operators {
+    public static void main(String[] args) {
+        int plus = 10+20;
+        int minus = 20-10;
+        int multiply = 20*10;
+        int divide = 20/10;
+        int modulo = 15%10;
+        
+    }
+}
