2c127adb87c78681fdcca68e1a65c6726632aced
Jang2723 <jangsb46@gmail.com>
2023-11-21 10:55:50 +0900

    add all Files

--- src/D2Operators.java
+++ src/D2Operators.java
@@ -5,6 +5,22 @@ public class D2Operators {
         int multiply = 20*10;
         int divide = 20/10;
         int modulo = 15%10;
-        
+        System.out.println(plus);       //30
+        System.out.println(minus);      //10
+        System.out.println(multiply);   //200
+        System.out.println(divide);     //2
+        System.out.println(modulo);     //5
+
+        // 사칙연산의 우선순의
+        int result = 10 + 5 * 3;
+        System.out.println(result);
+        //괄호가 최우선
+        result = (10 + 5) * 3;
+        System.out.println(result);
+
+        // %의 우선순위는?
+        result = (10 + 5 % 3) /3 + 2 * 3;
+        System.out.println(result);
+
     }
 }
