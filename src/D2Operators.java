0245e118774a534030a62df1c64e01d2226db557
Jang2723 <jangsb46@gmail.com>
2023-11-21 14:52:17 +0900

    update D2Operators and add Q3~6

--- src/D2Operators.java
+++ src/D2Operators.java
@@ -58,5 +58,79 @@ public class D2Operators {
         System.out.println("여기에 10을 입력하시오: " + 10);
         System.out.println(10 + " + " + 10 + " = " + 20);
         String formula = 10 + " + " + 10 + " = " + 20;
+
+        char largeA = 'A';
+        char ten = 10;
+        System.out.println((char) (largeA + ten));
+
+        int count = 0;
+        count++;
+        System.out.println("현재 횟수 : " + count);
+        count++;
+        System.out.println("현재 횟수 : " + count);
+        count--;
+        System.out.println("현재 횟수 : " + count);
+        ++count;
+        --count;
+        System.out.println("최종 : " + count);
+
+        count = 10;
+        System.out.println(++count); //11 (출력 전에 1 증가, 값 11)
+        System.out.println(count++); //11 (출력 후에 1 증가, 값 12)
+        System.out.println(--count); //11 (출력 전에 1 감소, 값 11)
+        System.out.println(count--); //11 (출력 후에 1 감소. 깂 10)
+
+        int addTo = 1;
+        addTo += 10;        // 11
+        addTo *= addTo;     // 121
+
+        addTo /= 12;        // addTo = addTo / 12;
+        addTo %= 10;        // addTo = addTo % 10;
+        addTo -= 20;        // addTo = addTo - 20;
+        addTo *= -1;        // addTo = addTo * -1;
+
+        System.out.println(addTo);
+
+        // 비교 연산자
+        int small1 = 10;
+        int small2 = 10;
+        int big = 21;
+        double bigD = 21.0;
+        System.out.println(small1 == small2);   //true
+        System.out.println(small1 == big);      //false
+        System.out.println(big == bigD);        //true
+        System.out.println(small1 > small2);    //false
+        System.out.println(small1 >= small2);   //true
+        System.out.println(small1 != small2);
+        System.out.println(small1 <= small2);
+        System.out.println(small2 < bigD);
+
+        // 논리 연산자
+        int temperature = 38;
+        int age = 20;
+
+        // 체온은 37 이하
+        boolean notSick = temperature <= 37;
+        // 나이는 20 이상
+        boolean isOfAge = age >= 20;
+        // 입장 가능한가?
+        boolean canEnter = notSick && isOfAge;
+        System.out.println(canEnter);
+
+        // 일요일 이거나 굥휴일이다.
+        // 일요일이다.
+        boolean sunday = false;
+        // 공휴일이다.
+        boolean holiday = false;
+        // 쉬는 날이다.
+        boolean canRest = sunday || holiday;
+        System.out.println(canRest);
+
+        // 아니다.
+        int number = 10;
+        boolean isNegative  = number < 10;
+        boolean isNotNegative = !isNegative;
+        System.out.println(isNegative);
+        System.out.println(isNotNegative);
     }
 }
---
+++ src/d2prob/Q6Operators.java
@@ -0,0 +1,16 @@
+package d2prob;
+
+import java.util.Scanner;
+
+/*세개의 양의 정수 A, B, C를 입력받아서,
+A가 B와 C의 공배수인지를 true 또는 false로 출력하여라.*/
+public class Q6Operators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        boolean isCommonMultiple = a % b ==0 && a % c == 0;
+        System.out.println(isCommonMultiple);
+    }
+}
