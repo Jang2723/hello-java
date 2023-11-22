ff5fd4c85cc092ff08705c8f78075c5b23347e4a
Jang2723 <jangsb46@gmail.com>
2023-11-22 09:35:45 +0900

    update H5op

--- src/d2prob/H5op.java
+++ src/d2prob/H5op.java
@@ -9,9 +9,20 @@ public class H5op {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();                                        //2 일경우
+        /*
         System.out.println(String.format("A^2의 1의 자리 : %1d",a*a%10 ));     //4
         System.out.println(String.format("A^4의 1의 자리 : %1d",a*a*a*a%10)); //6
         System.out.println(String.format("A^8의 1의 자리 : %1d",a*a*a*a*a*a*a*a%10)); //6
+        */
+        a *= a;
+        a %= 10;
+        System.out.println(a);
+        a *= a;
+        a %= 10;
+        System.out.println(a);
+        a *= a;
+        a %= 10;
+        System.out.println(a);
 
     }
 }
